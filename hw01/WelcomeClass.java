fd4f21a9fb4fe085cfb89229df73761a324281b7
Kevin Trinh <ket216@lehigh.edu>
2014-08-31 21:42:11 +0000

    WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+/*  Kevin Trinh
+    CSE 2
+    HW01 - WelcomeClass
+    August 28, 2014
+*/
+
+// define a class
+public class WelcomeClass{
+    
+    // add main method
+    public static void main(String[] args){
+        
+        // print welcome sign
+        System.out.println("    -----------");
+        System.out.println("    | WELCOME |");
+        System.out.println("    -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-K--E--T--2--1--6-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("This summer, I participated in a Lehigh internship called BDSI. I learned about yeast genetics.");
+    }
+}
