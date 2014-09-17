edadf9fbd94161bf46bd256585f1ce5b35dd1be8
Kevin Trinh <ket216@lehigh.edu>
2014-09-17 01:21:04 +0000

    hw03

--- hw03/FourDigits.java
+++ hw03/FourDigits.java
@@ -15,12 +15,17 @@ public class FourDigits {
         Scanner digit = new Scanner (System.in);    // declares digit as a Scanner from system input
         System.out.println("Enter a double: ");     // prompts user to enter a double
         double fourDigit = digit.nextDouble();      // stores user's double into fourDigit
+        int tenths;                                 // declares variable for tenths place
+        int hundredths;                             // declares variable for hundredths place
+        int thousandths;                            // declares variable for thousandths place
+        int tenthousandths;                         // declares variable for ten thousandths place
         
-        int left = (int)fourDigit;                  // converts fourDigit to integer (digits left of decimal)
-        double right = (fourDigit-left)*10000;      // "removes" digits left of decimal and * 10,000 so four digits are now left of decimal
-        int rightDigits = (int)right;               // converts double right to int rightDigits
+        tenths=(int)(fourDigit*10)%10;              // calculates 10ths place
+        hundredths=(int)(fourDigit*100)%10;         // calculates 100ths place
+        thousandths=(int)(fourDigit*1000)%10;       // calculates 1000ths place
+        tenthousandths=(int)(fourDigit*10000)%10;   // calculates 10000ths place
         
         // final print
-        System.out.println("The four digits are: " +rightDigits +".");
+        System.out.println("The four digits are: " + tenths + hundredths + thousandths + tenthousandths + ".");
     }
 }
